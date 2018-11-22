538f5b1cf6d052235671c1227ce52426241fe297
viganhajdari <vigan.hajdarii@gmail.com>
2018-11-22 16:23:43 +0100

    Integrated action for portoflio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,8 +31,8 @@ puts "5 skills created"
         title: "Portfolio title #{portfolio_item}",
         subtitle:"my great service",
         body:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        main_image:"https://via.placeholder.com/700x350",
-        thumb_image:"https://via.placeholder.com/150x350"
+        main_image:"https://via.placeholder.com/500x350",
+        thumb_image:"https://via.placeholder.com/350x150"
     )
 
 end
